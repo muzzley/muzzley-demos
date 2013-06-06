2c7d77e51a166951e6cdc63f957804512218e145
Tania Rocha <tania.rocha@muzzley.com>
2013-06-06 13:00:35 +0100

    add new demos: Image widget, volume and motion components

---
+++ javascript/components/motion-sensor/js/rotate-box.js
@@ -0,0 +1,18 @@
+var init = function() {
+      var box = document.querySelector('.container').children[0],
+      showPanelButtons = document.querySelectorAll('#show-buttons button'),
+      panelClassName = 'show-front',
+
+      onButtonClick = function( event ){
+        box.removeClassName( panelClassName );
+        panelClassName = event.target.className;
+        box.addClassName( panelClassName );
+      };
+
+  for (var i=0, len = showPanelButtons.length; i < len; i++) {
+    showPanelButtons[i].addEventListener( 'click', onButtonClick, false);
+  }
+  
+};
+  
+window.addEventListener( 'DOMContentLoaded', init, false);
