7afac3da5120d18774705fd13131a72cd726db5a
Hugo Sousa <hugo.sousa@muzzley.com>
2013-10-28 15:24:38 +0000

    [FIX] [Compass.js] - Change the degree calculation logic in  Rotate() function.

--- javascript/widgets/Gamepad/js/compass.js
+++ javascript/widgets/Gamepad/js/compass.js
@@ -10,19 +10,7 @@ function clearCanvas() {
 }
 
 function rotate(){
-	if(degrees == 0){
-		degrees = 90;
-	}else if (degrees == 90) {
-		degrees = 0;
-	}else if (degrees == 135) {
-		degrees = 315;
-	}else if (degrees == 180) {
-		degrees = 270;
-	}else if (degrees == 270) {
-		degrees = 180;
-	}else if (degrees == 315) {
-		degrees = 135;
-	}
+	degrees = 360 - (degrees - 90);	
 	draw();
 }
 
